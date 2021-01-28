0e97dee5792012656192d4f3beaeca38f90f1d02
soferdani <soferdani@gmail.com>
2021-01-28 16:40:56 +0200

    serch component is reaqdy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      this is react-weather-app
+      <Search/>
     </div>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+
+export default function Search() {
+
+    
+    return (
+    <div className="search">
+            <input placeholder='City Name'></input>
+            <button>Search</button>
+    </div>
+    );
+}
