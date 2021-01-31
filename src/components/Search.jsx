8c0cfddea1ae0365aff0ecfab9401eee6827c1bb
soferdani <soferdani@gmail.com>
2021-01-31 13:17:50 +0200

    hendel input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 
 export default function Search() {
 
-    
+    const [cityFromUser, setCityFromUser] = useState('')
+
+    const handelCityChange = function (event) {
+        setCityFromUser(event.target.value)
+    }
+
+    const handelCitySearch = function () {
+        console.log(cityFromUser);
+        setCityFromUser('')
+    }
+
     return (
     <div className="search">
-            <input placeholder='City Name'></input>
-            <button>Search</button>
+            <input onChange={handelCityChange} value={cityFromUser} placeholder='City Name'></input>
+            <button onClick={handelCitySearch} >Search</button>
     </div>
     );
 }
