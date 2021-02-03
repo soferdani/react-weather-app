ef32fcfcca04141c70e0cba55a24d91bf61ae12c
soferdani <soferdani@gmail.com>
2021-02-03 13:37:26 +0200

    render options from api

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,31 +5,47 @@ const WEATHER_API_KEY= process.env.REACT_APP_WEATHER_API
 
 export default function Search() {
     const [cityFromUser, setCityFromUser] = useState('')
+    const [citesFromAPI, setCitesFromAPI] = useState(null)
+    const [currentGeoLocation, setCurrentGeoLocation] = useState(null)
 
-
-    useEffect(() => {
+    useEffect(() => { // at the moment does not really working like it should
         const cityKey = async function () {
-            let data = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${WEATHER_API_KEY}&q=${cityFromUser}`)
-            data = await data.data
-            console.log(data);
+            let LocationAPI = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${WEATHER_API_KEY}&q=${cityFromUser}`)
+            LocationAPI = await LocationAPI.data
+            setCitesFromAPI(LocationAPI)
         }
         cityKey()
+    }, [cityFromUser]) 
 
-    },[cityFromUser])
+
+    useEffect(() => { // good format for the api req of the geolocation is 
+        ///http://dataservice.accuweather.com/locations/v1/cities/geoposition/search.json?q=32.1,34.8&apikey=ip1VDGLRf1bg4vLPoUeea9yP7gJPQ6BX&toplevel=false
+
+        
+        navigator.geolocation.getCurrentPosition(console.log, console.log) 
+    },[])
 
     const handelCityChange = function (event) {
-        setCityFromUser(event.target.value)
+            setCityFromUser(event.target.value)
     }
 
     const handelCitySearch = function () {
-        console.log(cityFromUser);
         setCityFromUser('')
     }
+
 
     return (
     <div className="search">
             <input onChange={handelCityChange} value={cityFromUser} placeholder='City Name'></input>
+            <select name='cites'>
+                {citesFromAPI ? citesFromAPI.map(city => 
+                    <option key={city.Key} value={city.Key}>{city.LocalizedName}</option>
+                ): <option value='null'>Loading</option>}
+            </select>
             <button onClick={handelCitySearch} >Search</button>
+
+            
     </div>
     );
 }
+
